a5bf3ceb634899aec7332dd4d8f09879711286a1
jerrickhoang <jerrickhoang@gmail.com>
2014-04-17 22:14:56 -0400

    Created basic Goal class

---
+++ src/Goal.java
@@ -0,0 +1,27 @@
+
+public class Goal {
+	private int x;
+	private int y;
+	
+	public Goal(int x, int y) {
+		setX(x); setY(y);
+	}	
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
